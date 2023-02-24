ef232bc6a0aa2fddb56d075257937686da863006
D1ffic00lt <dm.filinov@gmail.com>
2023-02-25 03:24:54 +0700

    started working on the CSVReader class

--- checker/units/reader.py
+++ checker/units/reader.py
@@ -1,12 +1,41 @@
 import os
+import pandas as pd
 
 
-class Reader(object):
+class ReadObject(object):
+    def __init__(self, host: str, ports: str):
+        self.host = host
+        self.ports = ports
+
+    def get_request_type(self):
+        pass
+
+    def __repr__(self) -> str:
+        return "{0}({1}, {2})".format(self.__module__, self.host, self.ports)
+
+    def __call__(self) -> list:
+        return []
+
+
+class CSVReader(object):
     def __init__(self, filename: str):
         self.filename = filename
+        self.units = self.read()
 
-    def get_file_exists_status(self):
+    def get_file_exists_status(self) -> bool:
         return os.path.exists(self.filename)
 
-    def __repr__(self):
+    def read(self):
+        if self.get_file_exists_status():
+            data = pd.read_csv(self.filename, sep=";")
+            return [ReadObject(host, ports) for host, ports in data.values.tolist()]
+        return []
+
+    def __repr__(self) -> str:
         return "{0}({1})".format(self.__module__, self.filename)
+
+    def __str__(self):
+        return "[{0}]".format(", ".join([repr(obj) for obj in self.units]))
+
+    def __call__(self):
+        yield from self.units
