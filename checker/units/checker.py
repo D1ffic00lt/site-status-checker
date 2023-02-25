d9f14579a588467025f8981487bcbefb58fe162e
D1ffic00lt <dm.filinov@gmail.com>
2023-02-25 12:41:31 +0700

    add load function to class Checker

--- checker/units/checker.py
+++ checker/units/checker.py
@@ -1,6 +1,14 @@
+from typing import List
+
+from reader import CSVReader
+
+
 class Checker(object):
     def __init__(self):
-        pass
+        self.data: List[CSVReader] = []
+
+    def load(self, reader: CSVReader):
+        self.data.append(reader)
 
     def __repr__(self):
         return "Checker()"
