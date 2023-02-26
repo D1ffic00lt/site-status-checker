0e59cce7a5b824680e4feea1da06cd3bc09f2bd7
D1ffic00lt <dm.filinov@gmail.com>
2023-02-26 21:29:54 +0300

    add __call__ function to class Checker

--- checker/config.py
+++
@@ -1,3 +0,0 @@
-FORMAT = '[%(asctime)s] [%(levelname)s]: %(process)d %(name)s %(message)s'
-DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-LOG_PATH = "logs.log"
--- checker/units/exceptions.py
+++ checker/units/exceptions.py
@@ -4,8 +4,8 @@ from functools import wraps
 
 
 class IgnoreInternetExceptions(object):
-    def __init__(self):
-        pass
+    def __init__(self, check_ip: bool = False):
+        self.check_ip = check_ip
 
     def __repr__(self):
         return "IgnoreInternetExceptions()"
@@ -14,8 +14,13 @@ class IgnoreInternetExceptions(object):
         @wraps(func)
         def decorator(*args, **kwargs):
             try:
-                return func(*args, **kwargs)
+                result = func(*args, **kwargs)
+                if self.check_ip:
+                    return True
+                return result
             except requests.exceptions.ConnectionError:
+                if self.check_ip:
+                    return False
                 return "ConnectionError"
 
         return decorator
